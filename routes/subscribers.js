0cf2a23c1dc8d9438bdb71ecbbcbe212e6e7fb50
Derek <109226053+derekjtong@users.noreply.github.com>
2023-10-24 09:18:27 -0400

    Added PATCH and DELETE

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -13,8 +13,8 @@ router.get("/", async (req, res) => {
 });
 
 // Getting one
-router.get("/:id", (req, res) => {
-  res.send("Info for " + req.params.id);
+router.get("/:id", getSubscriber, (req, res) => {
+  res.send(res.subscriber);
 });
 
 // Creating one
@@ -33,9 +33,44 @@ router.post("/", async (req, res) => {
 
 // Updating one
 // not put. put will relace everything, patch will just replace what was input.
-router.patch("/", (req, res) => {});
+router.patch("/:id", getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // Deleting one
-router.delete("/", (req, res) => {});
+router.delete("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.deleteOne();
+    res.json({ message: "Deleted subscriber" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: "Cannot find subscriber" });
+    }
+  } catch (err) {
+    return status(500).json({ message: err.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
